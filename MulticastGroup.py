812e12e7de852ee64947168bd58511df8394bfcd
tobi-ctrl <tobiasmeinhardt@live.de>
2020-06-11 19:42:33 +0200

    clear server_addresses

--- MulticastGroup.py
+++ MulticastGroup.py
@@ -1,6 +1,7 @@
 import socket
 import struct
 import sys
+import numpy as np
 
 #IP Multicast group
 MULTICAST_GROUP="224.0.0.0"
@@ -9,7 +10,7 @@ MULTICAST_SERVER_ADDR = ('', 10000)
 class MulticastGroup:
     def __init__(self):
         self.group = []
-
+        self.server_addresses = []
         #Create UDP socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         #Bind to the server address
@@ -24,6 +25,8 @@ class MulticastGroup:
         print('\nwaiting to receive message')
         data, address = self.sock.recvfrom(1024)
         server_address = address[0]
+        if not server_address in self.server_addresses:
+            self.server_addresses.append(server_address)
 
         print('received %s bytes from %s' % (len(data), address))
         print(data)
@@ -32,7 +35,8 @@ class MulticastGroup:
         self.sock.sendto(b'ack', address)
 
         #Create group
-        if not server_address in self.group:
-            self.group.append(server_address)
+        self.group = self.server_addresses
+
+        self.server_addresses = []
         return self.group
         
