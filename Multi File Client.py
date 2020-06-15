22f69ef9eda333faf8e618879a9956abf026d2a0
tobi-ctrl <tobiasmeinhardt@live.de>
2020-06-15 17:47:24 +0200

    multicast as heartbeat

--- Multi File Client.py
+++ Multi File Client.py
@@ -6,32 +6,56 @@ Tolga Camlice
 """
 
 import socket
+import struct
+import sys
 import os
 import time
 import multiprocessing
 from Checksum import Checksum
 from pathlib import Path
 
-SERVER_IP = "127.0.0.1"
+#IP Multicast group
+MULTICAST_GROUP="224.0.0.0"
+MULTICAST_SERVER_ADDR = ('', 10000)
 NUMBER_CLIENTS = 3
 #Define a timeout for connections
 TIMEOUT = 5000
 #Receive buffer size
 BUFFER_SIZE = 1024
-FILENAME = Path("C:/DistributedSystem/test_received.txt")
+#Use path of FritzBox NAS space 
+FILENAME = Path("Y:/Gastbereich/test.txt")
 CS = Checksum(FILENAME)
+
+def create_tcp_socket():
+    try: 
+        #Create a TCP socket
+        print('Create TCP socket')
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.settimeout(TIMEOUT)
+        return client_socket
+    except socket.error:
+        print("Error creating tcp socket")
+
+def create_udp_socket():
+    try: 
+        #Create a UDP socket
+        print('Create UDP socket')
+        multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        multicast_socket.bind(MULTICAST_SERVER_ADDR)
+        #Tell the operating system to add the socket to the multicast group
+        #on all interfaces.
+        group = socket.inet_aton(MULTICAST_GROUP)
+        mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+        multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+        return multicast_socket
+    except socket.error:
+        print("Error creating udp socket")
 
 def send_message(client_id, server_address, server_port):
     client_id += 1
     print('This is client ' + str(client_id) + ' with process id ' + str(os.getpid()))
-    try:
-        #Create a TCP socket
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.settimeout(TIMEOUT)
 
-    except socket.error:
-        print("Error creating socket")
-
+    client_socket = create_tcp_socket()
     try:
         client_socket.connect((server_address, server_port))
 
@@ -92,18 +116,35 @@ def send_message(client_id, server_address, server_port):
         print('Socket closed')
         
 if __name__ == "__main__":
-    server_address = SERVER_IP
+    server_address = ""
     server_port = 3000
     p = None
 
-    #Start 1 process of each client
-    for i in range(NUMBER_CLIENTS):
-        client_id = i 
-        p = multiprocessing.Process(target=send_message, args=(client_id, server_address, server_port))
-        p.start()
-        p.join
-    time.sleep(3)
+    multicast_socket = create_udp_socket()
+
+    while True:
+        print('Waiting for message on multicast channel...')
+        data, address = multicast_socket.recvfrom(1024)
+        print('received %s from %s' % (data, address))
+        multicast_socket.sendto(b'ack', address)
+        server_address = address[0]
+        if data:
+            break
+        
+    try:
+        #Start 1 process of each client
+        for i in range(NUMBER_CLIENTS):
+            client_id = i 
+            p = multiprocessing.Process(target=send_message, args=(client_id, server_address, server_port))
+            p.start()
+            p.join
+    except KeyboardInterrupt:
+        print("caught keyboard interrupt, exiting")
+        p.terminate()
+        sys.exit(1)
+
+"""     time.sleep(3)
     print("finished")
-    p.terminate()
+    p.terminate() """
     
 
--- Multi Request File Server.py
+++ Multi Request File Server.py
@@ -7,15 +7,28 @@ Tolga Camlice
 
 import multiprocessing
 import socket
-import tqdm
+import struct
+import time
 import os
 import sys
 from Checksum import Checksum
+from MulticastGroup import MulticastGroup
 from pathlib import Path
+import threading
 
+#Pass Server ID string from command line argument
+SERVER_ID = "1"
+if((len(sys.argv) - 1) >= 1):
+    SERVER_ID = sys.argv[1]  
+#Transfer socket
 IP=socket.gethostbyname(socket.gethostname())
-FILENAME = Path("C:/DistributedSystem/test.txt")
+TCP_PORT = 3000
+#IP Multicast group with UDP socket port
+MULTICAST_GROUP=("224.0.0.0", 10000)
+#Use path of FritzBox NAS space 
+FILENAME = Path("Y:/Gastbereich/test.txt")
 CS = Checksum(FILENAME)
+MG = MulticastGroup()
 # get the file size
 FILESIZE = os.path.getsize(FILENAME)
 #Receive buffer size
@@ -28,37 +41,56 @@ class Server(multiprocessing.Process):
         self.received_data = received_data
         self.msg = self.received_data.decode()
         self.client_address = client_address
-        self.server_id = 1
 
     def run(self):
-        print('This is server ' + str(self.server_id) + ' with process id ' + str(os.getpid()))
+        print('This is server ' + SERVER_ID + ' with process id ' + str(os.getpid()))
         if self.received_data and self.msg=='file':
             #Transmit file over socket
             send_file(self.connection, self.client_address)
+
+#Thread 
+class Multicast(object):
+        def __init__(self, *args):
+                """ Constructor
+                :type interval: int
+                :param interval: Check interval, in seconds
+                """
+                self.args = args
+                thread = threading.Thread(target=self.run, args=())
+                thread.daemon = True                            # Daemonize thread
+                thread.start()                                  # Start the execution
+
+        def run(self):
+            try:
+                while True:
+                    #Send data to the multicast group
+                    multicast_message = b'Server ' + bytes(SERVER_ID, 'utf-8')
+                    print("Send message to multicast group: ", multicast_message)
+                    sent = multicast_socket.sendto(multicast_message, MULTICAST_GROUP)
+                    time.sleep(1)
+                    #Update the group view
+                    group_view = MG.update_group()
+                    print(group_view)
+            except KeyboardInterrupt:
+                print("caught keyboard interrupt, exiting")
+
 
 def send_file(connection, client_address):
     checksum = CS.generate_digest()
     
     print("sending file", f"{FILENAME}".encode(), "with filesize", FILESIZE, "kb", "to client", client_address)
     print("\n")
-    progress = tqdm.tqdm(range(FILESIZE), f"Sending {FILENAME}", unit="B", unit_scale=True, unit_divisor=1024)
     with open(FILENAME, "rb") as f:
-        for _ in progress:
-            # read the bytes from the file
-            bytes_read = f.read(BUFFER_SIZE)
-            if not bytes_read:
-                # file transmitting is done
-                break
-            
-            try:
-                # we use sendall to assure transimission in 
-                # busy networks
-                connection.sendall(bytes_read)
-                # update the progress bar
-                progress.update(len(bytes_read))
+        # read the bytes from the file
+        bytes_read = f.read(BUFFER_SIZE)
+        
+        try:
+            # we use sendall to assure transimission in 
+            # busy networks
+            connection.sendall(bytes_read)
                 
-            except socket.error:
-                print("Error sending file")
+        except socket.error:
+            print("Error sending file")
 
         try:
             connection.send(b"checksum=" + str.encode(checksum))
@@ -79,23 +111,44 @@ def read_from_socket(connection, client_address):
     except socket.error:
         print("Error receiving data")
 
-if __name__ == "__main__":
-    port = 3000
+def create_tcp_socket():
     try: 
         #Create a TCP socket
+        print('Create TCP socket')
         listener_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #Accept connections from any host
-        listener_socket.bind(("", port))
+        listener_socket.bind(("", TCP_PORT))
         listener_socket.listen()
-    
+        return listener_socket
     except socket.error:
-        print("Error creating socket")
+        print("Error creating tcp socket")
 
-    print('Server up and running at {}:{}'.format(IP, port))
+def create_udp_socket():
+    try: 
+        #Create a UDP socket
+        print('Create UDP socket')
+        multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        multicast_socket.settimeout(20.0)
+        #Set the time-to-live for messages to 1 so they do not go past the
+        #local network segment.
+        ttl = struct.pack('b', 1)
+        multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+        return multicast_socket
+    except socket.error:
+        print("Error creating udp socket")
 
+if __name__ == "__main__":
+    #Create transfer socket 
+    listener_socket = create_tcp_socket()
+
+    print('Server up and running at {}:{}'.format(IP, TCP_PORT))
+
+    multicast_socket = create_udp_socket()
+    Multicast()
+    
     try:
         while True:
-            print('\nWaiting to receive message...\n')
+            print('\nWaiting to receive message on tcp socket...\n')
             connection, client_address = listener_socket.accept()
             read_from_socket(connection, client_address)
             
